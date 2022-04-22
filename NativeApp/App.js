50dc47aeb051ea84b2a42f9a8211e38c6a1d772d
nguyenhuuphuc.cse <nguyenhuuphuc.cse@gmail.com>
2022-04-22 17:09:10 +0700

    add login UI

--- NativeApp/App.js
+++ NativeApp/App.js
@@ -2,6 +2,7 @@ import { NativeBaseProvider } from "native-base";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+import { Login, Signup } from "./components/Auth";
 import DeviceInfo from "./pages/DeviceInfo";
 import ViewDevice from './pages/ViewDevice';
 import EditPolicy from "./pages/editPolicy";
@@ -17,34 +18,38 @@ export default function App() {
     <GardenContextProvider>
     <NativeBaseProvider>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Root/MainApp/Homepage">
+        <Stack.Navigator 
+          initialRouteName="Root/Auth/Login"
+          screenOptions={{headerShown: false}}
+        >
+          <Stack.Screen 
+            name="Root/Auth/Login" 
+            component={Login} 
+          />
+          <Stack.Screen 
+            name="Root/Auth/Signup" 
+            component={Signup} 
+          />
           <Stack.Screen 
             name="Root/MainApp/Homepage" 
             component={Homepage} 
-            options={{ headerShown: false }}
           />
           <Stack.Screen 
             name="Root/MainApp/AddGarden" 
             component={AddGarden} 
-            options={{ headerShown: false }}
           />
           <Stack.Screen
             name="Root/MainApp/EditPolicy"
             component={EditPolicy}
-            options={{ headerShown: false }}
           />
           <Stack.Screen
             name="Root/MainApp/ViewEngine"
             component={ViewDevice}
-            options={{ headerShown: false }}
           />
           <Stack.Screen
             name="Root/MainApp/DeviceInfo"
             component={DeviceInfo}
-            options={{ headerShown: false }}
           />
-          {/* <Stack.Screen name="Root/MainApp/Policy" component={Policy} /> */}
-          {/* <Stack.Screen name="Root/MainApp/Scheduler" component={Scheduler} /> */}
         </Stack.Navigator>
       </NavigationContainer>
     </NativeBaseProvider>
